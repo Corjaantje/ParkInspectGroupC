bef439da0bf08611d1e2d0c7317defe56db62b0d
mikerovers <mikerovers97@gmail.com>
2016-11-04 14:59:43 +0100

    Changed account table

--- DatabaseProject/Tables/Account.sql
+++ DatabaseProject/Tables/Account.sql
@@ -3,5 +3,7 @@
     [Id] INT NOT NULL,  
     [Username] VARCHAR(25) NOT NULL, 
     [Password] VARCHAR(50) NOT NULL, 
+    [Employee] BIT NULL, 
+    [Customer] BIT NULL, 
     CONSTRAINT [PK_Account] PRIMARY KEY ([Id]) 
 )
