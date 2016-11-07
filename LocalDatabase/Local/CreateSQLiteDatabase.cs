5c27710c997d48424cea41fcdc1f746407697603
RogierB <rogier.bazelmans@gmail.com>
2016-11-07 22:35:06 +0100

    Added local db tables: Region, EmployeeStatus, Employee, Availability

--- LocalDatabase/Local/CreateSQLiteDatabase.cs
+++ LocalDatabase/Local/CreateSQLiteDatabase.cs
@@ -22,6 +22,18 @@ namespace LocalDatabase.Local
                 createtable = Account(_sqliteActions, _connection);
                 if (!createtable) return false;
 
+                createtable = Region(_sqliteActions, _connection);
+                if (!createtable) return false;
+
+                createtable = EmployeeStatus(_sqliteActions, _connection);
+                if (!createtable) return false;
+
+                createtable = Employee(_sqliteActions, _connection);
+                if (!createtable) return false;
+
+                createtable = Availability(_sqliteActions, _connection);
+                if (!createtable) return false;
+
                 return true;
             }
             else
@@ -46,13 +58,81 @@ namespace LocalDatabase.Local
         private bool Account(DatabaseActions _sqliteActions, SQLiteConnection _connection)
         {
             string sql = "CREATE TABLE Account (" +
-                "Id INTEGER PRIMARY KEY," +
+                "Id INT PRIMARY KEY," +
                 "Username VARCHAR(25) NOT NULL," +
                 "Password VARCHAR(25) NOT NULL," +
                 "UserGuid VARCHAR(50) NOT NULL," +
                 "DateCreated datetime NOT NULL," +
                 "DateUpdated datetime NOT NULL," +
                 "ExistsInCentral INT NOT NULL" +
+                ")";
+
+            bool action = _sqliteActions.CUD(_connection, sql);
+            return action;
+        }
+        private bool Region(DatabaseActions _sqliteActions, SQLiteConnection _connection)
+        {
+            string sql = "CREATE TABLE Region (" +
+                "Id INT NOT NULL PRIMARY KEY," +
+                "Region VARCHAR(50) NOT NULL," +
+                "DateCreated datetime NOT NULL," +
+                "DateUpdated datetime NOT NULL," +
+                "ExistsInCentral INT NOT NULL" +
+                ")";
+
+            bool action = _sqliteActions.CUD(_connection, sql);
+            return action;
+        }
+        private bool EmployeeStatus(DatabaseActions _sqliteActions, SQLiteConnection _connection)
+        {
+            string sql = "CREATE TABLE EmployeeStatus (" +
+                "Id INT NOT NULL PRIMARY KEY," +
+                "Description VARCHAR(25) NOT NULL" +
+                ")";
+
+            bool action = _sqliteActions.CUD(_connection, sql);
+            return action;
+        }
+        private bool Employee(DatabaseActions _sqliteActions, SQLiteConnection _connection)
+        {
+            string sql = "CREATE TABLE Employee (" +
+                "Id INT NOT NULL PRIMARY KEY," +
+                "FirstName VARCHAR(30) NOT NULL," +
+                "Prefix VARCHAR(10) NULL," +
+                "SurName VARCHAR(30) NOT NULL," +
+                "Gender CHAR NULL," +
+                "City VARCHAR(20) NULL, " +
+                "Address VARCHAR(50) NULL, " +
+                "ZipCode VARCHAR(10) NULL, " +
+                "Phonenumber VARCHAR(15) NULL, " +
+                "Email VARCHAR(50) NULL," +
+                "RegionId INT NULL," +
+                "EmployeeStatusId INT NULL," +
+                "Inspecter BIT NOT NULL, " +
+                "Manager BIT NOT NULL, " +
+                "AccountId INT NOT NULL, " +
+                "DateCreated datetime NOT NULL," +
+                "DateUpdated datetime NOT NULL," +
+                "ExistsInCentral INT NOT NULL," +
+                "FOREIGN KEY(RegionId) REFERENCES Region(Id)," +
+                "FOREIGN KEY(EmployeeStatusId) REFERENCES EmployeeStatus(Id)," +
+                "FOREIGN KEY(AccountId) REFERENCES Account(Id)" +
+                ")";
+
+            bool action = _sqliteActions.CUD(_connection, sql);
+            return action;
+        }
+        private bool Availability(DatabaseActions _sqliteActions, SQLiteConnection _connection)
+        {
+            string sql = "CREATE TABLE Availability (" +
+                "EmployeeId INT," +
+                "Date DATE NULL," +
+                "EndTime TIME(0) NULL,"+ 
+                "StartTime TIME(0) NULL," +
+                "DateCreated datetime NOT NULL," +
+                "DateUpdated datetime NOT NULL," +
+                "ExistsInCentral INT NOT NULL," +
+                "FOREIGN KEY(EmployeeId) REFERENCES Employee(Id)" +
                 ")";
 
             bool action = _sqliteActions.CUD(_connection, sql);
