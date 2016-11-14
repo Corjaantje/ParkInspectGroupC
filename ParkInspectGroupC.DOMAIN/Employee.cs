d84f15f7f5cead87d2f57feda9b0ce20ef629fba
Mike Rovers <mikerovers97@gmail.com>
2016-11-14 08:13:18 +0100

    Database updates week 1 (#12)
    * Manager aan Employee toegevoegd.
    
    Kaart #card-774810
    
    * Default aan Password toegevoegd
    
    Kaart #card-774185
    https://github.com/Corjaantje/ParkInspectGroupC/projects/1#card-774185
    
    * Default EmployeeStatus toegevoegd
    
    Kaart #card-770964
    https://github.com/Corjaantje/ParkInspectGroupC/projects/1#card-770964
    
    * Updated Post sql scripts.
    
    * ADO aangepast naar nieuwe database updates van WK1
    
    * Employee cleanup

--- ParkInspectGroupC.DOMAIN/Employee.cs
+++ ParkInspectGroupC.DOMAIN/Employee.cs
@@ -22,6 +22,7 @@ namespace ParkInspectGroupC.DOMAIN
             this.Availability = new HashSet<Availability>();
             this.Inspection = new HashSet<Inspection>();
             this.WorkingHours = new HashSet<WorkingHours>();
+            this.Employees = new HashSet<Employee>();
         }
     
         public int Id { get; set; }
@@ -36,10 +37,11 @@ namespace ParkInspectGroupC.DOMAIN
         public string Email { get; set; }
         public Nullable<int> RegionId { get; set; }
         public Nullable<int> EmployeeStatusId { get; set; }
-        public bool Inspecter { get; set; }
-        public bool Manager { get; set; }
         public System.DateTime DateCreated { get; set; }
         public System.DateTime DateUpdated { get; set; }
+        public bool IsInspecter { get; set; }
+        public bool IsManager { get; set; }
+        public Nullable<int> ManagerId { get; set; }
     
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<Account> Account { get; set; }
@@ -53,5 +55,8 @@ namespace ParkInspectGroupC.DOMAIN
         public virtual ICollection<Inspection> Inspection { get; set; }
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<WorkingHours> WorkingHours { get; set; }
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<Employee> Employees { get; set; }
+        public virtual Employee Manager { get; set; }
     }
 }
