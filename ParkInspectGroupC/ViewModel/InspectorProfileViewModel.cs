394846bdd5d6304db0845e786f22675a828ab22c
Roykovic <Roy@isnecfan.nl>
2016-11-16 17:11:06 +0100

    added inspectorprofile view model

---
+++ ParkInspectGroupC/ViewModel/InspectorProfileViewModel.cs
@@ -0,0 +1,15 @@
+﻿using ParkInspectGroupC.DOMAIN;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParkInspectGroupC.ViewModel
+{
+   public class InspectorProfileViewModel
+    {
+       }
+
+    }
+}
