244c546e06c8fe425cab1c73d36ec4f3a0bed070
Roykovic <Roy@isnecfan.nl>
2016-11-16 19:18:55 +0100

    added attributes to viewmodel and view

--- ParkInspectGroupC/ViewModel/InspectorProfileViewModel.cs
+++ ParkInspectGroupC/ViewModel/InspectorProfileViewModel.cs
@@ -17,16 +17,44 @@ namespace ParkInspectGroupC.ViewModel
            get { return _name; }
            set { _name = value; RaisePropertyChanged("Name"); }
        }
+
+       private string _adress;
+       public string Adress
+       {
+           get { return _adress; }
+           set { _adress = value; RaisePropertyChanged("Adress"); }
+       }
+       private string _email;
+       public string Email
+       {
+           get { return _email; }
+           set { _email = value; RaisePropertyChanged("Email"); }
+       }
+
+       private string _phoneNumber;
+       public string PhoneNumber
+       {
+           get { return _phoneNumber; }
+           set { _phoneNumber = value; RaisePropertyChanged("PhoneNumber"); }
+       }
+
+       private string _status;
+       public string Status
+       {
+           get { return _status; }
+           set { _status = value; RaisePropertyChanged("Status"); }
+       }
        public InspectorProfileViewModel() 
        {
-           Name = "Roy Tersluijsen";
             //TODO The emp variable should be a parameter to this constructor
            using (var context = new ParkInspectEntities())
            {
                Emp = (from e in context.Employee where e.Id == 1 select e).FirstOrDefault();
            }
-           Name = Emp.FirstName + " " + Emp.Prefix + " " + Emp.SurName;
-           Console.WriteLine("Name: " + Name);
+           Name = Emp.FirstName + " " + Emp.Prefix + " " + Emp.SurName + "(" + Emp.Gender + ")";
+           Adress = Emp.Address + ", " + Emp.ZipCode + ", " + Emp.City;
+           Email = Emp.Email;
+           PhoneNumber = Emp.Phonenumber;
        }
 
     }
