157b1bc9358dee099d186a79333d1306f4d9613e
mikerovers <mikerovers97@gmail.com>
2016-11-16 15:00:05 +0100

    Thema selecter
    Er kunnen nu thema's geselecteerd worden.

--- ParkInspectGroupC/ViewModel/MainViewModel.cs
+++ ParkInspectGroupC/ViewModel/MainViewModel.cs
@@ -1,34 +1,39 @@
+using System;
+using System.Linq;
+using System.Windows;
 using GalaSoft.MvvmLight;
+using ParkInspectGroupC.Miscellaneous;
+using Simple.Wpf.Themes;
+using System.Collections.Generic;
 
 namespace ParkInspectGroupC.ViewModel
 {
-    /// <summary>
-    /// This class contains properties that the main View can data bind to.
-    /// <para>
-    /// Use the <strong>mvvminpc</strong> snippet to add bindable properties to this ViewModel.
-    /// </para>
-    /// <para>
-    /// You can also use Blend to data bind with the tool's support.
-    /// </para>
-    /// <para>
-    /// See http://www.galasoft.ch/mvvm
-    /// </para>
-    /// </summary>
     public class MainViewModel : ViewModelBase
     {
-        /// <summary>
-        /// Initializes a new instance of the MainViewModel class.
-        /// </summary>
-        public MainViewModel()
-        {
-            ////if (IsInDesignMode)
-            ////{
-            ////    // Code runs in Blend --> create design time data.
-            ////}
-            ////else
-            ////{
-            ////    // Code runs "for real"
-            ////}
-        }
+		private Theme _selectedTheme;
+		public IEnumerable<Theme> Themes { get; private set; }
+
+		public Theme SelectedTheme
+		{
+			get
+			{
+				return _selectedTheme;
+			}
+			set
+			{
+				_selectedTheme = value;
+				RaisePropertyChanged("SelectedTheme");
+			}
+		}
+
+		public MainViewModel()
+	    {
+			Themes = new[]
+			{
+				new Theme("No theme (default)", null),
+				new Theme("Expression Dark", new Uri("/ParkInspectGroupC;component/Themes/ExpressionDark.xaml", UriKind.Relative)),
+				new Theme("Shiny Blue", new Uri("/ParkInspectGroupC;component/Themes/ShinyBlue.xaml", UriKind.Relative))
+			};
+		}
     }
 }
