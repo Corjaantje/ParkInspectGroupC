40fe5b253fde55bad3a49c4d599edee4914009a5
Roykovic <Roy@isnecfan.nl>
2016-11-21 19:16:57 +0100

    init GMAP

--- ParkInspectGroupC/ViewModel/MapViewModel.cs
+++ ParkInspectGroupC/ViewModel/MapViewModel.cs
@@ -1,12 +1,29 @@
-﻿using System;
+﻿using GMap.NET.WindowsPresentation;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace ParkInspectGroupC.ViewModel
 {
     public class MapViewModel
     {
+        GMapControl gmap { get; set; }
+
+        public MapViewModel()
+        {
+            gmap = new GMapControl();
+            gmap.Zoom = 5;
+            gmap.MaxZoom = 15;
+            gmap.MinZoom = 1;
+            gmap.MapProvider = GMap.NET.MapProviders.BingMapProvider.Instance;
+            GMap.NET.GMaps.Instance.Mode = GMap.NET.AccessMode.ServerOnly;
+            gmap.SetPositionByKeywords("Netherlands");
+            gmap.CenterCrossPen = null;
+            gmap.DragButton = MouseButton.Left;
+
+        }
     }
 }
