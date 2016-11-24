cd888d5478e9d1dcdc6f75fe2106753a6ed36c30
Roykovic <Roy@isnecfan.nl>
2016-11-24 23:29:20 +0100

    added showinspections command + button

--- ParkInspectGroupC/ViewModel/MapViewModel.cs
+++ ParkInspectGroupC/ViewModel/MapViewModel.cs
@@ -1,4 +1,5 @@
-﻿using GMap.NET.WindowsPresentation;
+﻿using GalaSoft.MvvmLight.CommandWpf;
+using GMap.NET.WindowsPresentation;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,12 +11,22 @@ namespace ParkInspectGroupC.ViewModel
 {
     public class MapViewModel
     {
+        public ICommand ShowInspectionsCommand { get; set; }
         public GMapControl gmap { get; set; }
 
         public MapViewModel()
         {
-         
-
+            ShowInspectionsCommand = new RelayCommand(ShowInspections, CanShowInspections);
         }
+        private void ShowInspections()
+        {
+           
+        }
+
+        private bool CanShowInspections()
+        {
+            
+            return true;
+        }    
     }
 }
