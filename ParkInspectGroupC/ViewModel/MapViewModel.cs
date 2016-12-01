dc393c942369bdfb17d081d8e4477f9ff0d09d1d
Roykovic <Roy@isnecfan.nl>
2016-12-01 16:45:17 +0100

    see last commit

--- ParkInspectGroupC/ViewModel/MapViewModel.cs
+++ ParkInspectGroupC/ViewModel/MapViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using GalaSoft.MvvmLight.CommandWpf;
 using GMap.NET.WindowsPresentation;
+using ParkInspectGroupC.DOMAIN;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,29 +15,11 @@ namespace ParkInspectGroupC.ViewModel
     public class MapViewModel
     {
         public ICommand ShowInspectionsCommand { get; set; }
-        public GMapControl gmap { get; set; }
-        public IEnumerable<GMapMarker> markers { get; set; }
         public IEnumerable<GMapMarker> Markers { get; set; }
 
-        public MapViewModel()
         int frequency;
         public MapViewModel(IEnumerable<Inspection> PInspections)
         {
-            var point = new GMap.NET.PointLatLng(52.855864177854, 9.140625);
-            Console.WriteLine(point);
-            GMapMarker marker = new GMapMarker(point);
-            markers = new List<GMapMarker>();
-            marker.Shape = new Ellipse
-            {
-                Width = 10,
-                Height = 10,
-                Stroke = Brushes.Black,
-                StrokeThickness = 1.5
-            };
-            markers = markers.Concat(new[] { marker });
-            point = new GMap.NET.PointLatLng(51.80438169999999, 5.760212799999977);
-            marker = new GMapMarker(point);
-            marker.Shape = new Ellipse
 
             AddInspections(PInspections);
         }
@@ -64,18 +47,11 @@ namespace ParkInspectGroupC.ViewModel
             Markers = Markers.Concat(new[] { marker });
             marker.Shape = new Ellipse
             {
-                Width = 10,
                 Width = frequency * 10,
                 Height = 10,
                 Stroke = Brushes.Black,
                 StrokeThickness = 1.5
             };
-            markers = markers.Concat(new[] { marker });
-            ShowInspectionsCommand = new RelayCommand(ShowInspections, CanShowInspections);
-        }
-        private void ShowInspections()
-        {
-           
             Markers = Markers.Concat(new[] { marker });
         }
 
@@ -83,6 +59,11 @@ namespace ParkInspectGroupC.ViewModel
         {
             
             return true;
-        }    
+        }
+        public IEnumerable<Object> Inspections
+        {
+            get;
+            set; 
+        }
     }
 }
