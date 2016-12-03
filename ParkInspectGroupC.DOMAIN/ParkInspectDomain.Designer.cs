05a62b136f3970bc409268caf8705e5a03b7b80c
mikerovers <mikerovers97@gmail.com>
2016-12-03 20:48:19 +0100

    Rapport creatie toegevoegd

--- ParkInspectGroupC.DOMAIN/ParkInspectDomain.Designer.cs
+++ ParkInspectGroupC.DOMAIN/ParkInspectDomain.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'Z:\Github\ParkInspectGroupC\ParkInspectGroupC.DOMAIN\ParkInspectDomain.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\miker\Documents\GitHub\ParkInspectGroupC\ParkInspectGroupC.DOMAIN\ParkInspectDomain.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
--- ParkInspectGroupC/ViewModel/ReportCreation/ReportViewModel.cs
+++ ParkInspectGroupC/ViewModel/ReportCreation/ReportViewModel.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Input;
 using GalaSoft.MvvmLight;
 using GalaSoft.MvvmLight.CommandWpf;
@@ -19,10 +21,13 @@ namespace ParkInspectGroupC.ViewModel.ReportCreation
 		public ObservableCollection<Assignment> AssignmentList { get; set; }
 		public ObservableCollection<Report> ReportList { get; set; }
 		public ObservableCollection<ReportSection> ReportSectionList { get; set; }
+		public ObservableCollection<Diagram> ReportSectionDiagrams { get; set; }
+		public ObservableCollection<InspectionImage> ReportSectionImages { get; set; }
 
 		public Assignment SelectedAssignment { get; set; }
 		public Report SelectedReport { get; set; }
 		public Employee SelectedEmployee { get; set; }
+		public ReportSection SelectedReportSection { get; set; }
 
 		// Commands
 		public ICommand SaveReportCommand { get; set; }
@@ -30,20 +35,32 @@ namespace ParkInspectGroupC.ViewModel.ReportCreation
 		public ICommand OpenReportCommand { get; set; }
 		public ICommand DeleteReportCommand { get; set; }
 		public ICommand CreateReportCommand { get; set; }
+		public ICommand OpenReportSectionCommand { get; set; }
+		public ICommand DeleteReportSectionCommand { get; set; }
+		public ICommand CreateRepportSectionCommand { get; set; }
 
-		private string _reportTitle;
-		public string ReportTitle
+		public string ReportSectionTitle
 		{
-			get { return _reportTitle; }
-			set { _reportTitle = value; RaisePropertyChanged("ReportTitle"); }
+			get { return SelectedReportSection.Title; }
+			set { SelectedReportSection.Title = value; RaisePropertyChanged("ReportSectionTitle"); }
 		}
 
-		private string _reportDescription;
+		public string ReportSectionDescription
+		{
+			get { return SelectedReportSection.Summary; }
+			set { SelectedReportSection.Summary = value; RaisePropertyChanged("ReportSectionDescription"); }
+		} 
 
+		public string ReportTitle
+		{
+			get { return SelectedReport.Title; }
+			set { SelectedReport.Title = value; RaisePropertyChanged("ReportTitle"); }
+		}
+		
 		public string ReportDescription
 		{
-			get { return _reportDescription; }
-			set { _reportDescription = value; RaisePropertyChanged("ReportDescription"); }
+			get { return SelectedReport.Summary; }
+			set { SelectedReport.Summary = value; RaisePropertyChanged("ReportDescription"); }
 		}
 
 		public ReportViewModel()
@@ -51,13 +68,16 @@ namespace ParkInspectGroupC.ViewModel.ReportCreation
 			SaveReportCommand = new RelayCommand(SaveReport, CanSaveReport);
 			OpenAssignmentCommand = new RelayCommand(OpenAssignmentView);
 			OpenReportCommand = new RelayCommand(OpenRepport);
+			DeleteReportCommand = new RelayCommand(DeleteRepport);
 			CreateReportCommand = new RelayCommand(CreateReport);
+			OpenReportSectionCommand = new RelayCommand(OpenReportSection);
+			DeleteReportSectionCommand = new RelayCommand(DeleteRepportSection);
+			CreateRepportSectionCommand = new RelayCommand(CreateRepportSection);
 
 			using (var context = new ParkInspectEntities())
 			{
 				//TODO is voor test; weghalen!
 				SelectedEmployee = (from a in context.Employee where a.FirstName == "Jaqueline" select a).FirstOrDefault();
-
 
 				var assignmentList = (from a in context.Assignment.Include("Customer") where a.ManagerId == SelectedEmployee.Id select a).ToList();
 				AssignmentList = new ObservableCollection<Assignment>(assignmentList);
@@ -83,7 +103,7 @@ namespace ParkInspectGroupC.ViewModel.ReportCreation
 				Report report = new Report()
 				{
 					Title = "Tijdige titel",
-					Summary = "Tijdige samenvatting",
+					Summary = "Tijdige omschrijving",
 					Date = DateTime.Today,
 					AssignmentId = this.SelectedAssignment.Id,
 					EmployeeId = SelectedEmployee.Id
@@ -104,12 +124,71 @@ namespace ParkInspectGroupC.ViewModel.ReportCreation
 				ReportSectionList = new ObservableCollection<ReportSection>(reportSectionList);
 			}
 
+			ReportTitle = SelectedReport.Title;
+			ReportDescription = SelectedReport.Summary;
+
 			Navigator.SetNewView(new ReportView());
 		}
 
 		private void SaveReport()
 		{
-			
+			using (var context = new ParkInspectEntities())
+			{
+				// Report itself.
+				var report = (from a in context.Reports where a.Id == SelectedReport.Id select a).FirstOrDefault();
+
+				report.Title = ReportTitle;
+				report.Summary = ReportDescription;
+
+				// Report sections
+				foreach (var section in ReportSectionList)
+				{
+					var s = (from a in context.ReportSections where a.Id == section.Id select a).FirstOrDefault();
+
+					s.Title = section.Title;
+					s.Summary = section.Summary;
+					//s.Diagrams = section.Diagrams;
+					//s.InspectionImages = section.InspectionImages;
+
+					context.Entry(s).State = EntityState.Modified;
+				}
+
+				context.SaveChanges();
+			}
+		}
+
+		private void OpenReportSection()
+		{
+			using (var context = new ParkInspectEntities())
+			{
+				var section = (from a in context.ReportSections where a.Id == SelectedReportSection.Id select a).FirstOrDefault();
+
+				//ReportSectionTitle = section.Title;
+				//ReportSectionDescription = section.Summary;
+
+				//ReportSectionImages = new ObservableCollection<InspectionImage>(section.InspectionImages);
+				//ReportSectionDiagrams = new ObservableCollection<Diagram>(section.Diagrams);
+
+			}
+
+			Navigator.SetNewView(new ReportSectionView());
+		}
+
+		private void DeleteRepport()
+		{
+			MessageBoxResult messageBoxResult = MessageBox.Show("Weet je het zeker?", "Rapport Verwijder bevestiging",  MessageBoxButton.YesNo);
+			if (messageBoxResult == MessageBoxResult.Yes)
+			{
+				using (var context = new ParkInspectEntities())
+				{
+					var report = (from a in context.Reports where a.Id == SelectedReport.Id select a).FirstOrDefault();
+
+					context.Reports.Remove(report);
+					context.SaveChanges();
+
+					ReportList.Remove(SelectedReport);
+				}
+ 			}
 		}
 
 		private bool CanSaveReport()
@@ -118,6 +197,35 @@ namespace ParkInspectGroupC.ViewModel.ReportCreation
 				return false;
 
 			return true;
+		}
+
+		private void CreateRepportSection()
+		{
+			using (var context = new ParkInspectEntities())
+			{
+				var reportSection = new ReportSection()
+				{
+					Title = "Tijdige titel",
+					Summary = "Tijdige omschrijving",
+					ReportId = SelectedReport.Id
+				};
+
+				context.ReportSections.Add(reportSection);
+				context.SaveChanges();
+				ReportSectionList.Add(reportSection);
+			}
+		}
+
+		private void DeleteRepportSection()
+		{
+			using (var context = new ParkInspectEntities())
+			{
+				var reportSection = (from a in context.ReportSections where a.Id == SelectedReportSection.Id select a).FirstOrDefault();
+
+				context.ReportSections.Remove(reportSection);
+				context.SaveChanges();
+				ReportSectionList.Remove(SelectedReportSection);
+			}
 		}
 	}
 }
