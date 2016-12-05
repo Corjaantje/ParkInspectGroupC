7448181206006a4637d441c7aa48e0328af4a10e
Roykovic <Roy@isnecfan.nl>
2016-12-05 09:59:54 +0100

    test data init

--- ParkInspectGroupC/ViewModel/MapViewModel.cs
+++ ParkInspectGroupC/ViewModel/MapViewModel.cs
@@ -17,10 +17,17 @@ namespace ParkInspectGroupC.ViewModel
         public ICommand ShowInspectionsCommand { get; set; }
         public IEnumerable<GMapMarker> Markers { get; set; }
 
-        int frequency;
-        public MapViewModel(IEnumerable<Inspection> PInspections)
+        
+        public MapViewModel(
+           // IEnumerable<Inspection> PInspections
+            )
         {
-
+            IEnumerable<Inspection> PInspections = null;
+            using (var context = new ParkInspectEntities())
+            {
+                PInspections = (from i in context.Inspection   //Dummy data, solely used for testing
+                               select i).ToList();                      //TODO delete this and make the list come in throug parameter
+            }
             AddInspections(PInspections);
         }
 
