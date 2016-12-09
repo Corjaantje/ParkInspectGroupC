4e118e0696d79239a34019405319476c3888068a
Mike Rovers <mikerovers97@gmail.com>
2016-12-09 21:32:29 +0100

    Database update

---
+++ DatabaseProject/Tables/Coordinate.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Coordinate]
+(
+	[Id] INT IDENTITY(1,1) NOT NULL PRIMARY KEY, 
+    [Longitude] FLOAT NOT NULL, 
+    [Latitude] FLOAT NOT NULL, 
+    [Note] VARCHAR(MAX) NULL, 
+    [InspectionId] INT NOT NULL, 
+    CONSTRAINT [FK_Coordinate_Inspection] FOREIGN KEY ([InspectionId]) REFERENCES [Inspection]([Id])
+)
