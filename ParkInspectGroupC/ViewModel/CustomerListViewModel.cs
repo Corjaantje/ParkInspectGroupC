a5654551d561f080c3e8aa8acb017549c34d548a
RogierB <rogier.bazelmans@gmail.com>
2016-12-11 12:46:38 +0100

    Merge remote-tracking branch 'refs/remotes/origin/develop' into dbSync2
    # Conflicts:
    #	ParkInspectGroupC/ViewModel/CustomerListViewModel.cs

--- ParkInspectGroupC/ViewModel/CustomerListViewModel.cs
+++ ParkInspectGroupC/ViewModel/CustomerListViewModel.cs
@@ -1,12 +1,19 @@
 ﻿using GalaSoft.MvvmLight;
 using ParkInspectGroupC.DOMAIN;
-using System;
+
+using GalaSoft.MvvmLight.Command;
+using ParkInspectGroupC.DOMAIN;
+using ParkInspectGroupC.Miscellaneous;
+using ParkInspectGroupC.View;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+
+using System.Windows.Input;
+
 
 namespace ParkInspectGroupC.ViewModel
 {
@@ -15,6 +22,9 @@ namespace ParkInspectGroupC.ViewModel
         private ObservableCollection<Customer> _customers;
         private ObservableCollection<Customer> _allCustomers;
         private string _searchString = "Search";
+	private Customer _selectedCustomer;
+        public ICommand DeleteCustomerCommand { get; set; }
+        public ICommand AddCustomerCommand { get; set; }
 
         public CustomerListViewModel()
         {
@@ -22,20 +32,21 @@ namespace ParkInspectGroupC.ViewModel
 
             using (var context = new ParkInspectEntities())
             {
-                List<Customer> customers = context.Customers.ToList();
-
+		List<Customer> customers = context.Customers.ToList();
+		List<Customer> customers = context.Customer.ToList();
 
                 foreach (var customer in customers)
                 {
-        
-                    searchedCustomers.Add(customer);
-
+			searchedCustomers.Add(customer);
                 }
             }
 
             _allCustomers = searchedCustomers;
             _customers = searchedCustomers;
-        }
+	    DeleteCustomerCommand = new RelayCommand(deleteCustomer, canDelete);
+            AddCustomerCommand = new RelayCommand(openAddWindow);
+
+	}
         public ObservableCollection<Customer> Customers
         {
             get
@@ -50,7 +61,7 @@ namespace ParkInspectGroupC.ViewModel
             }
         }
 
-        public string SearchString
+	public string SearchString
         {
             get{
                 return _searchString;
@@ -78,5 +89,78 @@ namespace ParkInspectGroupC.ViewModel
                 RaisePropertyChanged("Customers");
             }
         }
-    }
+
+        public Customer SelectedCustomer
+        {
+            get { return _selectedCustomer; }
+            set
+            {
+                _selectedCustomer = value;
+            }
+        }
+
+        public string SearchString
+        {
+            get
+            {
+                return _searchString;
+            }
+
+            set
+            {
+                _searchString = value;
+
+
+                ObservableCollection<Customer> searchedCustomers = new ObservableCollection<Customer>();
+
+                foreach (var customer in _allCustomers)
+                {
+                    if (SearchString != null && customer.Name.ToLower().Contains(SearchString.ToLower()))
+                    {
+                        searchedCustomers.Add(customer);
+                    }
+                }
+
+
+                _customers = searchedCustomers;
+
+
+                RaisePropertyChanged("Customers");
+            }
+        }
+
+        public void deleteCustomer()
+        {
+            using (var context = new ParkInspectEntities())
+            {
+
+                List<Customer> customers = context.Customer.ToList();
+
+                foreach (var customer in customers)
+                {
+                    if (customer.Id == SelectedCustomer.Id)
+                    {
+                        context.Customer.Remove(customer);
+
+                    }
+                }
+                context.SaveChanges();
+            }
+
+            _customers.Remove(_selectedCustomer);
+            _allCustomers.Remove(_selectedCustomer);
+            RaisePropertyChanged("Customers");
+        }
+
+        public bool canDelete()
+        {
+            return true;
+        }
+
+        public void openAddWindow()
+        {
+            Navigator.SetNewView(new CustomerCreationView());
+        }
+
+	}
 }
