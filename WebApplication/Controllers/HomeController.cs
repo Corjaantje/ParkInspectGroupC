204dbb828cdc3f6769dbb1fef80ddb323410f928
RogierB <rogier.bazelmans@gmail.com>
2017-01-15 15:56:07 +0100

    Almost done, only the download of pdf's

---
+++ WebApplication/Controllers/HomeController.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ParkInspectGroupC.DOMAIN;
+
+namespace WebApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        bool redirect = false;
+        public ActionResult Index(string message)
+        {
+            if (message != "" && message != null)
+            {
+                ViewBag.Message = message;
+            }
+            
+            return View();
+        }
+
+        public ActionResult Overzicht(string Id)
+        {
+            bool found = false;
+            int id = Convert.ToInt32(Id);
+
+            using (var context = new ParkInspectEntities())
+            {
+                foreach (var client in context.Customer)
+                {
+                    if (client.Id == id)
+                    {
+                        found = true;
+                    }
+                }
+
+                if (found)
+                {
+                    //Get all the assigments from this customer
+                    List<int> AssigmentList = new List<int>();
+                    foreach (var ass in context.Assignment)
+                    {
+                        if (ass.CustomerId == id)
+                        {
+                            AssigmentList.Add(ass.Id);
+                        }
+                    }
+
+                    //Get all the reports from the assigments
+                    List<Report> ReportList = new List<Report>();
+                    foreach (var ass in AssigmentList)
+                    {
+                        foreach (var report in context.Report)
+                        {
+                            if (report.AssignmentId == ass)
+                            {
+                                ReportList.Add(report);
+                            }
+                        }
+                    }
+
+                    ViewBag.Id = id;
+                    return View(ReportList);
+                }
+                else
+                {
+                    return RedirectToAction("Index", "Home", new { @message = "Klant nummer bestaat niet!" });
+                }
+            }    
+        }
+
+        public ActionResult Pdf(string Rid, string Cid)
+        {
+            //Do pdf shit
+
+
+            //Return to view
+            return RedirectToAction("Overzicht", "Home", new { @Id = Cid });
+        }
+    }
+}
