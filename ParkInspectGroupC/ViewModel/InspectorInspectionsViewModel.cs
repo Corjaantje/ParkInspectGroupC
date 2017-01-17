4ec655f08b7cd024e9d06ac87c3b36e521ca090e
Jayduard Chapman <jayduard-c@hotmail.com>
2017-01-17 09:15:35 +0100

    Updates before twelve Req 53+54 (#83)
    * Buttons are in datagrid
    
    * Req nummer 53&54 voor manager via inpsectorlist

---
+++ ParkInspectGroupC/ViewModel/InspectorInspectionsViewModel.cs
@@ -0,0 +1,32 @@
+﻿using GalaSoft.MvvmLight;
+using LocalDatabase.Domain;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParkInspectGroupC.ViewModel
+{
+   public class InspectorInspectionsViewModel: ViewModelBase
+    {
+        private string _name;
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; RaisePropertyChanged("Name"); }
+        }
+
+        public ObservableCollection<Inspection> Inspections { get; set; }
+        public InspectorInspectionsViewModel(Employee selectedInspector)
+        {
+            Name = selectedInspector.FirstName + " " + selectedInspector.Prefix + " " + selectedInspector.SurName;
+            using(var context =  new LocalParkInspectEntities())
+            {
+                var IList = context.Inspection.Include("InspectionStatus").Where(i => i.InspectorId == selectedInspector.Id);
+                Inspections = new ObservableCollection<Inspection>(IList);
+            }
+        }
+    }
+}
